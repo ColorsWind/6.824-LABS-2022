532d5f43a0472170b4d43510a1ad18f02b266e0c
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2023-02-27 16:33:37 +0800

    rollback config.go

--- src/raft/config.go
+++ src/raft/config.go
@@ -589,7 +589,6 @@ func (cfg *config) one(cmd interface{}, expectedServers int, retry bool) int {
 			t1 := time.Now()
 			for time.Since(t1).Seconds() < 2 {
 				nd, cmd1 := cfg.nCommitted(index)
-				//log.Printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%v %v index=%v\n", nd, cmd, index)
 				if nd > 0 && nd >= expectedServers {
 					// committed
 					if cmd1 == cmd {
@@ -607,7 +606,7 @@ func (cfg *config) one(cmd interface{}, expectedServers int, retry bool) int {
 		}
 	}
 	if cfg.checkFinished() == false {
-		log.Panicf("one(%v) failed to reach agreement", cmd)
+		cfg.t.Fatalf("one(%v) failed to reach agreement", cmd)
 	}
 	return -1
 }
